31e794b14db678b150d8e009df3fb73befd57390
MiltonDavila2 <miltondaviladt@gmail.com>
2025-04-30 23:22:44 -0500

    Commit inicial
    Cosas del commit

---
+++ gestionlab/src/main/java/com/laboratorio/gestionlab/GestionlabApplication.java
@@ -0,0 +1,13 @@
+package com.laboratorio.gestionlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionlabApplication.class, args);
+	}
+
+}
---
+++ gestionlab/src/test/java/com/laboratorio/gestionlab/GestionlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laboratorio.gestionlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
